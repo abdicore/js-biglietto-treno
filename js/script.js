e04e8cf7cb061ab2c27eed0269d876b6f5930eba
abdicore <abdirhamanmohamed@gmail.com>
2024-07-31 15:45:04 +0200

    numero-km

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log ('js ok');
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+va applicato uno sconto del 20% per i minorenni
+
+va applicato uno sconto del 40% per gli over 65.
+
+
+*Nota
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+Questo richiederà un minimo di ricerca.*/
+
+
+/*
+1- chedere all utente i km
+2- calcolare il costo del viaggio in bbase alla tariffa kilometrica
+3- applicare gli sconti
+4- Mostrare in oagina il prezzo finale con i km
+*/
+
+
+
+// Fase di richiamo elementi del dom
+
+
+
+// Fase di raccolta dati (chidere all'utente i km da percorrere)
+
+const numberKm = parseInt(prompt('Quanti Kilomtreri vuoi percorrere?','30'))
+console.log(numberKm)
+
+
+// Fase elaborazione dati
+
+
+//Fase di output
